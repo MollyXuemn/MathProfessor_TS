fb50f06d4456648dc6c6e78f6777108abc1f5653
MollyXuemn <Molly504954366@gmail.com>
2023-11-15 21:23:15 +0100

    chore: add node_modules to gitignore

--- src/components/weekStatus.ts
+++ src/components/weekStatus.ts
@@ -1,9 +1,6 @@
 import { getClassStatus } from './classStatus';
 
 export function getWeekStatus(threshold: number, weekTimes: number[][]): string {
-    const totalClasses = weekTimes.length;
-    const cancelledClasses = weekTimes.filter((times) => getClassStatus(threshold, times) === 'YES').length;
 
-    const percentageCancelled = (cancelledClasses / totalClasses) * 100;
-    return `${percentageCancelled.toFixed(2)}%`;
+    return "";
 }
