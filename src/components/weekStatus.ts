377eb1cc3fdf35d4dd7ce4c8da9eb168f61f32c4
MollyXuemn <Molly504954366@gmail.com>
2023-11-15 21:24:46 +0100

    feat: weekStatus

--- src/components/weekStatus.ts
+++ src/components/weekStatus.ts
@@ -1,6 +1,9 @@
 import { getClassStatus } from './classStatus';
 
 export function getWeekStatus(threshold: number, weekTimes: number[][]): string {
+    const totalClasses = weekTimes.length;
+    const cancelledClasses = weekTimes.filter((times) => getClassStatus(threshold, times) === 'YES').length;
 
-    return "";
+    const percentageCancelled = (cancelledClasses / totalClasses) * 100;
+    return `${percentageCancelled.toFixed(2)}%`;
 }
